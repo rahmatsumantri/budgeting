0c2037dd29bcf7ec1065f856ba388814f1d01a42
Tri <rahmat.sumantri@romindo.net>
2023-09-29 15:53:05 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,9 @@ class DashboardController extends Controller
     public function index()
     {
         // get data
+        $from = date("Y-m-28", strtotime( date( "Y-m-d", strtotime( date("Y-m-d") ) ) . "-1 month" ) );
+        $to = date('Y-m-27');
+
         $outcomes = Outcome::get();
         $incomes = Income::get();
 
@@ -19,9 +22,7 @@ class DashboardController extends Controller
         $outcome_total = Outcome::sum('budget');
         $balance = $income_total - $outcome_total;
 
-        $outcome_month = Outcome::whereMonth('date', '=', date('m'))
-            ->whereYear('date', '=', date('Y'))
-            ->sum('budget');
+        $outcome_month = Outcome::whereBetween('date', [$from, $to])->sum('budget');
 
         // render view
         return view('dashboard', compact('incomes', 'outcomes', 'balance', 'outcome_month'));
